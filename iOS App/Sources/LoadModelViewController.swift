56dc557ef68ec1cd52b0771a171e66d94bd773a0
MasayoshiTsutsui <tsutsui-masayoshi0528@g.ecc.u-tokyo.ac.jp>
2023-06-23 18:48:51 +0900

    load mlmodel file succeeded

--- iOS App/Sources/LoadModelViewController.swift
+++ iOS App/Sources/LoadModelViewController.swift
@@ -13,9 +13,68 @@ class LoadModelViewController: UIViewController {
     @IBOutlet var loadModelButton: UIButton!
 
 
-
     @IBAction func loadLatestModel() {
         print("loadLatestModel called!")
-        self.performSegue(withIdentifier: "toMenuView", sender: nil)
+        self.loadModelButton.setTitle("Loading the latest model...", for: .normal)
+        self.loadModelButton.setTitleColor(.systemBlue, for: .normal)
+        
+        loadModelFromServer { success in
+            DispatchQueue.main.async {
+                if success {
+                    self.loadModelButton.setTitle("Load Successful", for: .normal)
+                    self.loadModelButton.setTitleColor(.systemGreen, for: .normal)
+                    print("最新のモデルがloadされました！")
+                } else {
+                    self.loadModelButton.setTitle("Load Failed", for: .normal)
+                    self.loadModelButton.setTitleColor(.systemRed, for: .normal)
+                    print("モデルの読み込みに失敗しました.")
+                }
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                    self.performSegue(withIdentifier: "toMenuView", sender: nil)
+                }
+            }
+        }
+    }
+    private func loadModelFromServer(completion: @escaping (Bool) -> Void) {
+        guard let url = URL(string: "https://mobile-federated-learning.com/load-model") else {
+            completion(false)
+            return
+        }
+
+        let task = URLSession.shared.downloadTask(with: url) { (url, response, error) in
+            if let error = error {
+                print("Error: \(error)")
+                completion(false)
+                return
+            }
+
+            // ファイルのダウンロードが成功した場合、urlに保存されているローカルファイルにアクセスできます
+            if let localURL = url {
+                let fileManager = FileManager.default
+                let destinationURL = self.getDestinationURL(for: "Hands2num_latest.mlmodel")
+                
+                do {
+                    try? fileManager.removeItem(at: destinationURL) // 既存のファイルがある場合は削除
+                    try fileManager.moveItem(at: localURL, to: destinationURL) // ファイルをリネームして移動
+                    print("Downloaded file: \(destinationURL.lastPathComponent)")
+                    completion(true)
+                } catch {
+                    print("Error moving file: \(error)")
+                    completion(false)
+                }
+            } else {
+                print("Error: Failed to download the file")
+                completion(false)
+            }
+        }
+        task.resume()
+    }
+
+    private func getDestinationURL(for fileName: String) -> URL {
+        // ファイルを保存するディレクトリのパスを取得
+        let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        // ファイル名を結合して保存先のURLを生成
+        let destinationURL = documentsDirectory.appendingPathComponent(fileName)
+        return destinationURL
     }
 }
